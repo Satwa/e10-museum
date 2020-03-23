9802271eed4bf7d5771f7e0f889920357de62a38
Joshua <satwa.fr@gmail.com>
2020-03-23 13:56:59 +0100

    adding basic three implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,72 @@
 import './style/app.styl'
 import * as THREE from 'three'
 
-console.log("Loaded")
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) => {
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+
+scene.add(new THREE.Mesh(
+    new THREE.TorusGeometry(1, .7, 12, 32),
+    new THREE.MeshNormalMaterial()
+))
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 8
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Resize
+ */
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Loop
+ */
+const loop = () => {
+    window.requestAnimationFrame(loop)
+
+    camera.position.x = cursor.x * 40
+    camera.position.y = -cursor.y * 40
+
+    camera.lookAt(scene.position)
+
+    // Render
+    renderer.render(scene, camera)
+}
+
+loop()
