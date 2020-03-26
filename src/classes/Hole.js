716f21de80dcafb2ca14979f05fd520bcd514dfb
Joshua <satwa.fr@gmail.com>
2020-03-26 21:29:49 +0100

    csg dependency (coucou l'état) + HoleDigger class

---
+++ src/classes/Hole.js
@@ -0,0 +1,24 @@
+import * as THREE from "three"
+import {ThreeBSP} from 'three-js-csg-es6'
+
+export default class HoleDigger {
+    constructor(originalGeometry, holeGeometry, material, holePosition = new THREE.Vector3()) {
+        const originalMesh = new ThreeBSP(new THREE.Mesh(originalGeometry))
+        
+        const _holeMesh = new THREE.Mesh(holeGeometry)
+        _holeMesh.position.set(holePosition.x, holePosition.y, holePosition.z)
+        
+        console.log(_holeMesh)
+        
+        const holeMesh     = new ThreeBSP(_holeMesh)
+
+        const theBoringGeometry = originalMesh.subtract(holeMesh) // do you have the ref, dear Elon?
+
+        this.mesh = theBoringGeometry.toMesh()
+        this.mesh.material = material
+    }
+
+    getMesh(){
+        return this.mesh
+    }
+}
